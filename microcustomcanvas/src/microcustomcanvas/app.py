c7a16a7a4ed9372acfef45171cb7f79f132fe700
Asheesh Laroia <asheesh@asheesh.org>
2021-04-29 20:52:21 -0700

    Add app that draws a circle

--- microcustomcanvas/src/microcustomcanvas/app.py
+++ microcustomcanvas/src/microcustomcanvas/app.py
@@ -1,25 +1,50 @@
 """
 Just a guy with dots and lines
 """
+from rubicon.java import JavaClass, JavaInterface
 import toga
-from toga.style import Pack
-from toga.style.pack import COLUMN, ROW
+import toga_android.widgets.base
+
+CustomView = JavaClass('org/beeware/android/CustomView')
+IView = JavaInterface('org/beeware/android/IView')
+Color = JavaClass("android/graphics/Color")
+Paint = JavaClass("android/graphics/Paint")
+
+
+class PlatformIndependentCanvasWidget:
+    @property
+    def _impl(self):
+        impl = getattr(self, '__impl', None)
+        if impl is None:
+            self.__impl = CanvasWidget()
+        return self.__impl
+
+
+class CanvasWidget:
+    def __init__(self):
+        super().__init__()
+        self.custom_view = CustomView(toga_android.widgets.base._get_activity().getApplicationContext())
+        self.custom_view.setView(MicroCustomCanvasView())
+
+    @property
+    def native(self):
+        return self.custom_view
+
+
+class MicroCustomCanvasView(IView):
+    def onDraw(self, canvas):
+        paint = Paint()
+        paint.setColor(Color.BLACK)
+        paint.setAntiAlias(True)
+        paint.setStrokeWidth(5.0)
+        canvas.drawOval(0.0, 0.0, 400.0, 400.0, paint)
 
 
 class MicroCustomCanvas(toga.App):
-
     def startup(self):
-        """
-        Construct and show the Toga application.
-
-        Usually, you would add your application to a main content box.
-        We then create a main window (with a name matching the app), and
-        show the main window.
-        """
-        main_box = toga.Box()
-
         self.main_window = toga.MainWindow(title=self.formal_name)
-        self.main_window.content = main_box
+        main_canvas = PlatformIndependentCanvasWidget()
+        self.main_window.content = main_canvas
         self.main_window.show()
 
 
